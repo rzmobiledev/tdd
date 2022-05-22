380f73a28aa41dd059654a735b3a7bd3bd9c1a92
rizal <rizal@rizals-MacBook-Pro.local>
2022-05-23 00:40:36 +0700

    core created

---
+++ app/admin/test.py
@@ -0,0 +1,19 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+from admin import calc
+
+class CalcTest(SimpleTestCase):
+    """Test calc module"""
+
+    def test_add_number(self):
+        """Test adding numbers together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_number(self):
+        """Test substracting numbers"""
+        res = calc.substract(10, 15)
+        self.assertEqual(res, 5)
