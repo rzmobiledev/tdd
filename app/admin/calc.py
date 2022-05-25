7384317428fdefe862ccbd0c5543ee2ff553900a
rizal <rizal@rizals-MacBook-Pro.local>
2022-05-25 23:22:20 +0700

    updating linting

--- app/admin/calc.py
+++ app/admin/calc.py
@@ -2,10 +2,12 @@
 Calculator function
 """
 
-def add(x,y):
+def add(x, y):
+
     """Add x and y and return result"""
     return x + y
 
 def substract(x,y):
+    
     """Substract x from y and return result"""
     return y - x
--- app/admin/test.py
+++ app/admin/test.py
@@ -5,12 +5,13 @@ from django.test import SimpleTestCase
 from admin import calc
 
 class CalcTest(SimpleTestCase):
+    
     """Test calc module"""
 
     def test_add_number(self):
+    
         """Test adding numbers together"""
-        res = calc.add(5,6)
-
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_substract_number(self):
