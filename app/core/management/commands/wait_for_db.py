827bb33960acee1749c0657b04fd42aa52a302c2
rizal <rizal@rizals-MacBook-Pro.local>
2022-05-26 11:19:02 +0700

    Updating wait_for_db file

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,8 +7,11 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 
 class Command(BaseCommand):
+
     """Django command to wait for database"""
+
     def handle(self, *args, **options):
+        
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
@@ -21,4 +24,3 @@ class Command(BaseCommand):
                 time.sleep(1)
         
         self.stdout.write(self.style.SUCCESS('Database is available'))
-
