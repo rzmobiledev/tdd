60cafe194acaddf2fb8d42e121ddba85a3e6969d
rizal <rizal@rizals-MacBook-Pro.local>
2022-05-26 13:47:40 +0700

    updating unit test

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,9 +7,8 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 
 class Command(BaseCommand):
-    
-    
     """Django command to wait for database"""
+    
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
